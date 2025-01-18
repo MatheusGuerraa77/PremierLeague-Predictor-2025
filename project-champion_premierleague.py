27784e593ed7ca676fe8d110cf36f5971a8b2ce2
MatheusGuerraa77 <matheusaraujoguerra2603@outlook.com>
2025-01-18 20:44:23 -0300

    Update project-champion_premierleague.py

--- project-champion_premierleague.py
+++ project-champion_premierleague.py
@@ -1,2 +1,12 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Título e descrição do app
+st.title("PremierLeague-Predictor 2025")
+st.markdown("""
+Um aplicativo interativo para visualizar dados da Premier League e estimar as probabilidades de título dos 12 primeiros times.
+""")
+
