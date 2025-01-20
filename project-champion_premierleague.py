a0c0fa2a125add0f1fdfd0e751cfee426d74f229
MatheusGuerraa77 <matheusaraujoguerra2603@outlook.com>
2025-01-20 19:41:57 -0300

    Update project-champion_premierleague.py

--- project-champion_premierleague.py
+++ project-champion_premierleague.py
@@ -5,8 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Título e descrição do app
+
 st.title("PremierLeague-Predictor 2025")
 st.markdown("""
 Um aplicativo interativo para visualizar dados da Premier League e estimar as probabilidades de título dos 12 primeiros times.
 """)
 
+# Função para carregar os dados
+def load_data(file):
+    return pd.read_csv(file)
+
+# Upload de dados
+uploaded_file = st.file_uploader("Carregar arquivo CSV com dados da Premier League", type=["csv"])
